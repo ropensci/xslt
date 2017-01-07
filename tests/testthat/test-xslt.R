238171d06ab42cc6e840aa2747801d54f88bce89
Jeroen Ooms <jeroenooms@gmail.com>
2017-01-07 16:49:02 +0100

    DOH

--- tests/testthat/test-xslt.R
+++ tests/testthat/test-xslt.R
@@ -4,6 +4,6 @@ test_that("XML document gets properly transformed using a stylesheet", {
   doc <- read_xml(system.file("examples/cd_catalog.xml", package = "xslt"))
   style <- read_xml(system.file("examples/cd_catalog.xsl", package = "xslt"))
   html <- xml_xslt(doc, style)
-  orig <- read_xml("tests/testthat/cd_catalog.html")
+  orig <- read_xml("cd_catalog.html")
   expect_equal(as.character(html, options = "as_html"), as.character(orig, options = "as_html"))
 })
