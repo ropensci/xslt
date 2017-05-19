5956ca40148d56a46618badce99ec8afe34bf699
Jeroen Ooms <jeroenooms@gmail.com>
2017-05-19 13:31:02 +0200

    Fix windows crash by copying doc

--- src/xslt.cpp
+++ src/xslt.cpp
@@ -8,13 +8,16 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 SEXP doc_xslt_apply(XPtrDoc doc, XPtrDoc xslt) {
-  xmlDocPtr docPtr = doc.checked_get();
-  xsltStylesheetPtr	xsltPtr = xsltParseStylesheetDoc(xslt.checked_get());
+  xmlDocPtr docPtr = xmlCopyDoc(doc.checked_get(), 1);
+  xmlDocPtr sheetPtr = xmlCopyDoc(xslt.checked_get(), 1);
+  xsltStylesheetPtr	xsltPtr = xsltParseStylesheetDoc(sheetPtr);
   xmlDocPtr res = xsltApplyStylesheet(xsltPtr, docPtr, NULL);
   if(xsltPtr->method && !strcmp("text", (char *) xsltPtr->method)){
     xmlChar * str; int len;
     xsltSaveResultToString(&str, &len, res, xsltPtr);
     return Rcpp::CharacterVector(std::string((char*) str, len));    
   }
+  xmlFreeDoc(docPtr);
+  xmlFreeDoc(sheetPtr);
   return XPtrDoc(res);
 }
