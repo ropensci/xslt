5cf38341a70f68a292d17748e9297d26b28fe5a4
Jeroen <jeroenooms@gmail.com>
2017-09-17 13:39:51 +0200

    Remove broken unit test

--- tests/testthat/test-param.R
+++ tests/testthat/test-param.R
@@ -4,7 +4,6 @@ test_that("parameters are rendered", {
   doc <- xml2::read_xml("doc.xml")
   style <- xml2::read_xml("style.xml")
   expect_equal(xml_xslt(doc, style), "author: \nreviewer: \n")
-  expect_equal(xml_xslt(doc, style, ""), "author: \nreviewer: \n")
   expect_equal(xml_xslt(doc, style, c(author="jeroen", reviewer="bert")), 
                "author: jeroen\nreviewer: bert\n")
 })
