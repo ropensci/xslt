3cdbe38c6062cd3fbf5a58a3fbe08d96431f849f
Jeroen <jeroenooms@gmail.com>
2017-09-19 14:12:29 +0200

    Fix pedantic warning

--- src/xslt.cpp
+++ src/xslt.cpp
@@ -11,10 +11,9 @@ SEXP doc_xslt_apply(XPtrDoc doc, XPtrDoc xslt, std::vector<std::string> params) 
   xmlDocPtr docPtr = xmlCopyDoc(doc.checked_get(), 1);
   xmlDocPtr sheetPtr = xmlCopyDoc(xslt.checked_get(), 1);
   xsltStylesheetPtr	xsltPtr = xsltParseStylesheetDoc(sheetPtr);
-  const char * strparam[params.size() + 1];
+  const char ** strparam = (const char **) calloc(params.size() + 1, sizeof(char*));
   for(size_t i = 0; i < params.size(); i++)
     strparam[i] = params.at(i).c_str();
-  strparam[params.size()] = NULL;
   xmlDocPtr res = xsltApplyStylesheet(xsltPtr, docPtr, strparam);
   if(xsltPtr->method && !strcmp("text", (char *) xsltPtr->method)){
     xmlChar * str; int len;
@@ -23,6 +22,7 @@ SEXP doc_xslt_apply(XPtrDoc doc, XPtrDoc xslt, std::vector<std::string> params) 
     xmlFreeDoc(sheetPtr);
     return Rcpp::CharacterVector(std::string((char*) str, len));    
   }
+  free(strparam);
   xmlFreeDoc(docPtr);
   xmlFreeDoc(sheetPtr);
   return XPtrDoc(res);
